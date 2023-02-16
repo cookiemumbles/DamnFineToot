ad6b91d1aa9bf3dd5c42678a88ee6f329c9f2ac7
cookie_mumbles <cookie_mumbles@proton.me>
2023-02-16 20:17:20 +0000

    feat: setup account + add starting point from example
    https://lib.rs/crates/mastodon-async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use mastodon_async::{helpers::{toml, cli}, Mastodon, Registration};
+use mastodon_async::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let read_file_result = toml::from_file("mastodon-data.toml");
+    let mastodon = if let Ok(data) = read_file_result {
+        Mastodon::from(data)
+    } else {
+        register().await?
+    };
+
+    let you = mastodon.verify_credentials().await?;
+
+    println!("{:#?}", you);
+    Ok(())
+}
+
+
+async fn register() -> Result<Mastodon> {
+    let registration = Registration::new("https://masto.ai")
+                                    .client_name("damnfinetoot")
+                                    .build()
+                                    .await?;
+    let mastodon = cli::authenticate(registration).await?;
+
+    // Save app data for using on the next run.
+    toml::to_file(&mastodon.data, "mastodon-data.toml")?;
+
+    Ok(mastodon)
 }
